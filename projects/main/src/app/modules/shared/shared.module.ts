d252c1fd36d232962c2cf63141cc80aa60a43d2d
unknown <alitarek868@yahoo.com>
2023-09-01 09:54:35 +0300

    add whats app icon

--- projects/main/src/app/modules/shared/shared.module.ts
+++ projects/main/src/app/modules/shared/shared.module.ts
@@ -1,21 +1,47 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TranslateModule } from '@ngx-translate/core';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { SectionTitleComponent } from './section-title/section-title.component';
 import { CarouselModule } from 'ngx-owl-carousel-o';
 import { ProductItemComponent } from './components/product-item/product-item.component';
 import { FooterComponent } from './footer/footer.component';
 import { NgxSliderModule } from 'ngx-slider-v2';
+import { SocialContactComponent } from './components/product-item/social-contact/social-contact.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 @NgModule({
-  declarations: [SectionTitleComponent, ProductItemComponent, FooterComponent],
-  imports: [CommonModule, CarouselModule],
+  declarations: [
+    SectionTitleComponent,
+    ProductItemComponent,
+    FooterComponent,
+    SocialContactComponent,
+  ],
+  imports: [
+    CommonModule,
+    CarouselModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      defaultLanguage: 'en',
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
+  ],
   exports: [
     TranslateModule,
     SectionTitleComponent,
     ProductItemComponent,
     FooterComponent,
     NgxSliderModule,
+    SocialContactComponent,
+    TranslateModule,
   ],
 })
 export class SharedModule {}
+// required for AOT compilation
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
